294c373be6bde45d95d362b572a5df44668ae1fa
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2021-12-25 21:37:38 +0000

    refactor: added router

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,26 @@
 import { GlobeIcon} from '@heroicons/react/solid'
 
 function Footer () {
-  return <footer>
-      <div>
+  return <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+      <div className="px-8 py-3">
        India
       </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
+        <div className="flex items-center justify-center md:col-span-2 lg:col-span-1 lg:col-start-2 ">
+         <GlobeIcon className="h-5 mr-1 text-green-700"/>Carbon neutral since 2007
+        </div>
+        <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+        <p>Advertising</p>
+        <p>Business</p>
+        <p>How search works</p>
+        </div>
+        <div className="flex justify-center space-x-8 md:ml-auto">
+        <p>Privacy</p>
+        <p>Terms</p>
+        <p>Settings</p>
+        </div>
+      </div>
+
     </footer>
   
 }
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,19 @@ import Avatar from "../components/Avatar"
 import { MicrophoneIcon ,ViewGridIcon} from '@heroicons/react/solid'
 import { SearchIcon } from '@heroicons/react/outline'
 import Footer from "../components/Footer"
+import { useRef } from "react"
+import { useRouter} from "next/router"
 
 export default function Home() {
+ const router = useRouter();
+ const searchInputRef = useRef(null)
+
+  const search = (e)=> {
+   e.preventDefault();
+   const term = searchInputRef.current.value;
+   if (!term) return;
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <Head>
@@ -28,21 +39,21 @@ export default function Home() {
        </div>
        </header>
 
-       <form className="flex flex-col items-center mt-38 flex-grow w-4/5">
+       <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
        <Image src="https://png2.cleanpng.com/sh/e1ddef276148a39234dd87af84cb8076/L0KzQYm3U8MxN5Nnj5H0aYP2gLBuTfdwd5hxfZ9yLXAwd7F2hBxmNZ10f9G2Z3Byd73sTcViapY1e6kEZUDkQIeCTsc1PWc7TqI8MUW1QoW3VMQ4O2o3SKM3cH7q/kisspng-google-i-o-google-logo-google-5abe0c79e0a069.7456660315224044739201.png"
        height={100}
        width={300}
        />
        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
        <SearchIcon className="h-5 mr-3 text-gray-500"/>
-       <input type="text" className="flex-grow focus:outline-none" />
+       <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
        <MicrophoneIcon className="h-5"/>
        </div>
        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-       <button className="btn">  Google Search </button>
-       <button className="btn"> I'm feeling Lucky </button>
+       <button onClick={search} className="btn">  Google Search </button>
+       <button  onClick={search} className="btn"> I'm feeling Lucky </button>
        </div>
-       <Footer />
+       <Footer/>
        </form>
        
     </div>
